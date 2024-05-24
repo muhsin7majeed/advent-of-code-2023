9c644e05f6b01538f06d49dd5805e7a0f1bc1a0a
muhsin <muhsin7ak@gmail.com>
2024-05-24 17:01:50 +0530

    Remove unused isNumber function

--- day-one.ts
+++ day-one.ts
@@ -27,22 +27,6 @@ Consider your entire calibration document. What is the sum of all of the calibra
  * 
  */
 
-/**
- * @param n - The input to check.
- * @returns {boolean} True if the input is a number, false otherwise.
- *
- * @description Check if given input is number or not.
- * ```
- * // These will return true:
- * "0", 0, "123"
- * // While these return false:
- * "abc123", "abc", NaN, []
- * ```
- */
-const isNumber = (n: any): boolean => {
-  return !isNaN(parseFloat(n)) && !isNaN(n - 0);
-};
-
 const inputString: string = dayOneInputString;
 
 /**
--- helpers/isNumber.ts
+++ helpers/isNumber.ts
@@ -1,3 +1,17 @@
-export const isNumber = (n: any) => {
+/**
+ * @param n - The input to check.
+ * @returns {boolean} True if the input is a number, false otherwise.
+ *
+ * @description Check if given input is number or not.
+ * ```
+ * // These will return true:
+ * "0", 0, "123"
+ * // While these return false:
+ * "abc123", "abc", NaN, []
+ * ```
+ */
+const isNumber = (n: any): boolean => {
   return !isNaN(parseFloat(n)) && !isNaN(n - 0);
 };
+
+export default isNumber;
